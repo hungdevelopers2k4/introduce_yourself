e45a356a353192ba38ef74dbe2418a00fb5db8e5
hà văn hưng <havanhung@192.168.6.101>
2025-08-20 17:17:49 +0700

    update

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,18 +1,17 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App"
 import Home from "../web/home"
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "/",
-        element:<Home/>
-      },
+        {
+            path: "/",
+            element:<Home/>
+        },
     ],
-  },
+    },
 ]);
-
 export default router;
