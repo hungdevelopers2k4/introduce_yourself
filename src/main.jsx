ae1e67262f3a5d43d0309dbb1713f23be8eb0ac5
hà văn hưng <havanhung@192.168.6.101>
2025-08-21 14:56:19 +0700

    upadte

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
+/** @jsxImportSource react */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css';
 import './sass/manager.scss'
 import router from "./router/router";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
